3f9fb452e3f9b7864db7c5c7560a72a2a0637a65
Kron <kron4ek@gmail.com>
2018-12-24 17:51:25 +0500

    Update build script
    Script is now more readable and easier to customize.

--- build_wine.sh
+++ build_wine.sh
@@ -1,73 +1,102 @@
 #!/bin/bash
 
-## Script for compiling Wine (vanilla, staging, esync, pba, proton).
-## It use two chroots for building (x32 chroot and x64 chroot).
+## Script for building Wine (vanilla, staging, esync, pba, proton).
+## It use two chroots for compiling (x32 chroot and x64 chroot).
+##
+## You can change env variables to desired values.
 ##
 ## Examples of how to use it:
 ##
-## ./build_wine.sh 4.0-rc2
-## ./build_wine.sh 4.0-rc2 staging
-## ./build_wine.sh 4.0-rc2 esync
-## ./build_wine.sh 3.16-5 proton
-## ./build_wine.sh 4.0-rc2 esync nopba exit
+## ./build_wine.sh 4.0-rc3						(build Wine 4.0-rc3)
+## ./build_wine.sh 4.0-rc3 exit					(download Wine sources and exit)
+## ./build_wine.sh 4.0-rc3 staging				(build Wine 4.0-rc3 with Staging patches)
+## ./build_wine.sh 4.0-rc3 esync				(build Wine 4.0-rc3 with ESYNC patches)
+## ./build_wine.sh 3.16-6 proton				(build latest Proton and name it 3.16-6)
+## ./build_wine.sh 4.0-rc3 esync pba fshack		(build WIne 4.0-rc3 with ESYNC, PBA and FSHACK patches)
 
-export USERHOME="/home/builder"
+export MAINDIR="$HOME"
+export SOURCES_DIR="$MAINDIR/sources_dir"
+export CHROOT_X64="$MAINDIR/xenial64_chroot"
+export CHROOT_X32="$MAINDIR/xenial_chroot"
+
+export C_COMPILER="gcc-8"
+export CXX_COMPILER="g++-8"
+
+export CFLAGS_X32="-march=pentium4 -O2"
+export CFLAGS_X64="-march=nocona -O2"
+export FLAGS_LD="-O2"
+export WINE_BUILD_OPTIONS="--without-coreaudio --without-curses --without-gstreamer --without-oss --disable-winemenubuilder --disable-tests --disable-win16"
+
+export ESYNC_VERSION="ce79346"
 
 build_in_chroot () {
-	CHR="$USERHOME/$1"
+	if [ "$1" = "32" ]; then
+		CHROOT_PATH="$CHROOT_X32"
+	else
+		CHROOT_PATH="$CHROOT_X64"
+	fi
 
-	mount --bind "$CHR" "$CHR"
-	mount --bind /dev "$CHR/dev"
-	mount --bind /dev/shm "$CHR/dev/shm"
-	mount --bind /dev/pts "$CHR/dev/pts"
-	mount --bind /proc "$CHR/proc"
-	mount --bind /sys "$CHR/sys"
+	echo "Unmount chroot directories. Just in case."
+	sudo umount -Rl "$CHROOT_PATH"
 
-	chroot "$CHR" /usr/bin/env LANG=en_US.UTF-8 TERM=xterm PATH="/bin:/sbin:/usr/bin:/usr/sbin" /root/build.sh
-	umount -Rl "$CHR"
+	echo "Mount directories for chroot"
+	sudo mount --bind "$CHROOT_PATH" "$CHROOT_PATH"
+	sudo mount --bind /dev "$CHROOT_PATH/dev"
+	sudo mount --bind /dev/shm "$CHROOT_PATH/dev/shm"
+	sudo mount --bind /dev/pts "$CHROOT_PATH/dev/pts"
+	sudo mount --bind /proc "$CHROOT_PATH/proc"
+	sudo mount --bind /sys "$CHROOT_PATH/sys"
+
+	echo "Chrooting into $CHROOT_PATH"
+	sudo chroot "$CHROOT_PATH" /usr/bin/env LANG=en_US.UTF-8 TERM=xterm PATH="/bin:/sbin:/usr/bin:/usr/sbin" /opt/build.sh
+
+	echo "Unmount chroot directories"
+	sudo umount -Rl "$CHROOT_PATH"
 }
 
-make_build_scripts () {
-	export FLAGS_ONE="-march=pentium4 -O2"
-	export FLAGS_TWO="-march=nocona -O2"
-	export FLAGSLD="-O2"
-	export CONFOPTIONS="--without-coreaudio --without-curses --without-gstreamer --without-oss --disable-winemenubuilder --disable-tests --disable-win16"
-	export COMPILERC="gcc-8"
-	export COMPILERCXX="g++-8"
+create_build_scripts () {
+	echo '#!/bin/sh' > $MAINDIR/build32.sh
+	echo 'cd /opt' >> $MAINDIR/build32.sh
+	echo 'export CC="'${C_COMPILER}'"' >> $MAINDIR/build32.sh
+	echo 'export CXX="'${CXX_COMPILER}'"' >> $MAINDIR/build32.sh
+	echo 'export CFLAGS="'${CFLAGS_X32}'"' >> $MAINDIR/build32.sh
+	echo 'export CXXFLAGS="'${CFLAGS_X32}'"' >> $MAINDIR/build32.sh
+	echo 'export LDFLAGS="'${FLAGS_LD}'"' >> $MAINDIR/build32.sh
+	echo 'mkdir build-tools && cd build-tools' >> $MAINDIR/build32.sh
+	echo '../wine/configure '${WINE_BUILD_OPTIONS}' --prefix /opt/wine32-build' >> $MAINDIR/build32.sh
+	echo 'make -j2' >> $MAINDIR/build32.sh
+	echo 'make install' >> $MAINDIR/build32.sh
+	echo 'export CFLAGS="'${CFLAGS_X64}'"' >> $MAINDIR/build32.sh
+	echo 'export CXXFLAGS="'${CFLAGS_X64}'"' >> $MAINDIR/build32.sh
+	echo 'cd ..' >> $MAINDIR/build32.sh
+	echo 'mkdir build-combo && cd build-combo' >> $MAINDIR/build32.sh
+	echo '../wine/configure '${WINE_BUILD_OPTIONS}' --with-wine64=../build64 --with-wine-tools=../build-tools --prefix /opt/wine-build' >> $MAINDIR/build32.sh
+	echo 'make -j2' >> $MAINDIR/build32.sh
+	echo 'make install' >> $MAINDIR/build32.sh
 
-	echo "#!/bin/sh" > "$USERHOME/xenial_chroot/root/build.sh"
-	echo "cd /root" >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo 'export CC="'$COMPILERC'"' >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo 'export CXX="'$COMPILERCXX'"' >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo 'export CFLAGS="'$FLAGS_ONE'"' >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo 'export CXXFLAGS="'$FLAGS_ONE'"' >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo 'export LDFLAGS="'$FLAGSLD'"' >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo "mkdir build-tools && cd build-tools" >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo "../wine/configure $CONFOPTIONS --prefix /root/wine32-build" >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo "make -j2" >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo "make install" >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo 'export CFLAGS="'$FLAGS_TWO'"' >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo 'export CXXFLAGS="'$FLAGS_TWO'"' >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo "cd .." >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo "mkdir build-combo && cd build-combo" >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo "../wine/configure $CONFOPTIONS --with-wine64=../build64 --with-wine-tools=../build-tools --prefix /root/wine-build" >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo "make -j2" >> "$USERHOME/xenial_chroot/root/build.sh"
-	echo "make install" >> "$USERHOME/xenial_chroot/root/build.sh"
+	echo '#!/bin/sh' > $MAINDIR/build64.sh
+	echo 'cd /opt' >> $MAINDIR/build64.sh
+	echo 'export CC="'${C_COMPILER}'"' >> $MAINDIR/build64.sh
+	echo 'export CXX="'${CXX_COMPILER}'"' >> $MAINDIR/build64.sh
+	echo 'export CFLAGS="'${CFLAGS_X64}'"' >> $MAINDIR/build64.sh
+	echo 'export CXXFLAGS="'${CFLAGS_X64}'"' >> $MAINDIR/build64.sh
+	echo 'export LDFLAGS="'${FLAGS_LD}'"' >> $MAINDIR/build64.sh
+	echo 'mkdir build64 && cd build64' >> $MAINDIR/build64.sh
+	echo '../wine/configure '${WINE_BUILD_OPTIONS}' --enable-win64 --prefix /opt/wine-build' >> $MAINDIR/build64.sh
+	echo 'make -j2' >> $MAINDIR/build64.sh
+	echo 'make install' >> $MAINDIR/build64.sh
 
-	echo "#!/bin/sh" > "$USERHOME/xenial64_chroot/root/build.sh"
-	echo "cd /root" >> "$USERHOME/xenial64_chroot/root/build.sh"
-	echo 'export CC="'$COMPILERC'"' >> "$USERHOME/xenial64_chroot/root/build.sh"
-	echo 'export CXX="'$COMPILERCXX'"' >> "$USERHOME/xenial64_chroot/root/build.sh"
-	echo 'export CFLAGS="'$FLAGS_TWO'"' >> "$USERHOME/xenial64_chroot/root/build.sh"
-	echo 'export CXXFLAGS="'$FLAGS_TWO'"' >> "$USERHOME/xenial64_chroot/root/build.sh"
-	echo 'export LDFLAGS="'$FLAGSLD'"' >> "$USERHOME/xenial64_chroot/root/build.sh"
-	echo "mkdir build64 && cd build64" >> "$USERHOME/xenial64_chroot/root/build.sh"
-	echo "../wine/configure $CONFOPTIONS --enable-win64 --prefix /root/wine-build" >> "$USERHOME/xenial64_chroot/root/build.sh"
-	echo "make -j2" >> "$USERHOME/xenial64_chroot/root/build.sh"
-	echo "make install" >> "$USERHOME/xenial64_chroot/root/build.sh"
+	chmod +x "$MAINDIR/build64.sh"
+	chmod +x "$MAINDIR/build32.sh"
 
-	chmod +x "$USERHOME/xenial64_chroot/root/build.sh"
-	chmod +x "$USERHOME/xenial_chroot/root/build.sh"
+	sudo mv "$MAINDIR/build64.sh" "$CHROOT_X64/opt/build.sh"
+	sudo mv "$MAINDIR/build32.sh" "$CHROOT_X32/opt/build.sh"
+}
+
+patching_error () {
+	clear
+	echo "Some patches were not applied correctly. Exiting."
+	exit
 }
 
 if [ ! "$1" ]; then
@@ -75,79 +104,74 @@ if [ ! "$1" ]; then
 	exit
 fi
 
-BUILD_DIR="$USERHOME/build_dir"
+if [ "$(echo "$1" | cut -c3)" = "0" ]; then
+	WINE_SOURCES_VERSION=$(echo "$1" | cut -c1).0
+else
+	WINE_SOURCES_VERSION=$(echo "$1" | cut -c1).x
+fi
 
-mkdir "$BUILD_DIR"
-cd "$BUILD_DIR" || exit
+rm -rf "$SOURCES_DIR"
+mkdir "$SOURCES_DIR"
+cd "$SOURCES_DIR" || exit
+
+clear
+echo "Downloading sources and patches."
+echo "Preparing Wine for compiling."
+echo
 
 if [ "$2" = "esync" ]; then
 	WINE_VERSION="$1-esync-staging"
-	ESYNC_VERSION="ce79346"
+	PATCHES_DIR="$SOURCES_DIR/PKGBUILDS/wine-tkg-git/wine-tkg-patches"
 
-	wget https://dl.winehq.org/wine/source/4.0/wine-$1.tar.xz
+	wget https://dl.winehq.org/wine/source/$WINE_SOURCES_VERSION/wine-$1.tar.xz
 	wget https://github.com/wine-staging/wine-staging/archive/v$1.tar.gz
 	wget https://github.com/zfigura/wine/releases/download/esync$ESYNC_VERSION/esync.tgz
-	wget https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/wine-tkg-git/wine-tkg-patches/esync-no_alloc_handle.patch
-	wget https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/wine-tkg-git/wine-tkg-patches/esync-staging-fixes-r3.patch
-	wget https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/wine-tkg-git/wine-tkg-patches/esync-compat-fixes-r3.patch
-	wget https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/wine-tkg-git/wine-tkg-patches/use_clock_monotonic.patch
-	wget https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/wine-tkg-git/wine-tkg-patches/FS_bypass_compositor.patch
-	wget https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/wine-tkg-git/wine-tkg-patches/valve_proton_fullscreen_hack-staging.patch
-	wget https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/wine-tkg-git/wine-tkg-patches/poe-fix.patch
-	wget https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/wine-tkg-git/wine-tkg-patches/steam.patch
-	wget https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/wine-tkg-git/wine-tkg-patches/CSMT-toggle.patch
-#	git clone https://github.com/Firerat/wine-pba.git
+	git clone https://github.com/Tk-Glitch/PKGBUILDS.git
+	git clone https://github.com/Firerat/wine-pba.git
 
-	tar xf wine-$1.tar.xz && mv wine-$1 wine
+	tar xf wine-$1.tar.xz
 	tar xf v$1.tar.gz
 	tar xf esync.tgz
 
-	# Apply some patches
-	cd wine
-	patch -Np1 < ../'use_clock_monotonic.patch'
-	patch -Np1 < ../'poe-fix.patch'
-	patch -Np1 < ../'steam.patch'
+	mv wine-$1 wine
 
-	cd "$BUILD_DIR/wine-staging-$1"
-	patch -Np1 < ../'CSMT-toggle.patch'
-	cd "$BUILD_DIR"
+	cd wine
+	patch -Np1 < "$PATCHES_DIR"/use_clock_monotonic.patch || patching_error
+	patch -Np1 < "$PATCHES_DIR"/poe-fix.patch || patching_error
+	patch -Np1 < "$PATCHES_DIR"/steam.patch || patching_error
+
+	cd ../wine-staging-$1
+	patch -Np1 < "$PATCHES_DIR"/CSMT-toggle.patch || patching_error
+	cd patches
+	./patchinstall.sh DESTDIR=../../wine --all || patching_error
+
 
 	# Apply fixes for esync patches
-	cd esync
-	patch -Np1 < ../'esync-staging-fixes-r3.patch'
-	patch -Np1 < ../'esync-compat-fixes-r3.patch'
-
-	# Apply staging patches
-	cd "$BUILD_DIR/wine-staging-$1/patches"
-	./patchinstall.sh DESTDIR="$BUILD_DIR/wine" --all
+	cd ../../esync
+	patch -Np1 < "$PATCHES_DIR"/esync-staging-fixes-r3.patch || patching_error
+	patch -Np1 < "$PATCHES_DIR"/esync-compat-fixes-r3.patch || patching_error
 
 	# Apply esync patches
-	cd "$BUILD_DIR/wine"
-	for f in "$BUILD_DIR/esync"/*.patch; do
-		git apply -C1 --verbose < "${f}"
+	cd ../wine
+	for f in ../esync/*.patch; do
+		git apply -C1 --verbose < "${f}" || patching_error
 	done
-	patch -Np1 < ../'esync-no_alloc_handle.patch'
+	patch -Np1 < "$PATCHES_DIR"/esync-no_alloc_handle.patch || patching_error
 
-	if [ "$3" != "nopba" ] && [ "$4" != "nopba" ] && [ "$5" != "nopba" ]; then
+	if [ "$3" = "pba" ] || [ "$4" = "pba" ] || [ "$5" = "pba" ]; then
 		WINE_VERSION="$WINE_VERSION-pba"
 
-		# Create pba patches
-		#mkdir -p "$BUILD_DIR/wine-pba/patches"
-		#cd "$BUILD_DIR/wine-pba"
-		#patch -Np1 < ../'PBA320+.patch'
-
 		# Apply pba patches
-		cd "$BUILD_DIR/wine"
-		for f in $(ls "$BUILD_DIR/wine-pba/patches"); do
-			patch -Np1 < "$BUILD_DIR/wine-pba/patches"/"${f}"
+		for f in $(ls ../wine-pba/patches); do
+			patch -Np1 < ../wine-pba/patches/"${f}" || patching_error
 		done
 	fi
 
 	if [ "$3" = "fshack" ] || [ "$4" = "fshack" ] || [ "$5" = "fshack" ]; then
 		WINE_VERSION="$WINE_VERSION-fshack"
 
-		patch -Np1 < ../'FS_bypass_compositor.patch'
-		patch -Np1 < ../'valve_proton_fullscreen_hack-staging.patch'
+		patch -Np1 < "$PATCHES_DIR"/FS_bypass_compositor.patch || patching_error
+		patch -Np1 < "$PATCHES_DIR"/valve_proton_fullscreen_hack-staging.patch || patching_error
 	fi
 elif [ "$2" = "proton" ]; then
 	WINE_VERSION="$1-proton"
@@ -156,9 +180,11 @@ elif [ "$2" = "proton" ]; then
 else
 	WINE_VERSION="$1"
 
-	wget https://dl.winehq.org/wine/source/4.0/wine-$1.tar.xz
+	wget https://dl.winehq.org/wine/source/$WINE_SOURCES_VERSION/wine-$1.tar.xz
 
-	tar xf wine-$1.tar.xz && mv wine-$1 wine
+	tar xf wine-$1.tar.xz
+
+	mv wine-$1 wine
 
 	if [ "$2" = "staging" ]; then
 		WINE_VERSION="$1-staging"
@@ -166,43 +192,58 @@ else
 		wget https://github.com/wine-staging/wine-staging/archive/v$1.tar.gz
 
 		tar xf v$1.tar.gz
+
 		cd wine-staging-$1/patches
-		./patchinstall.sh DESTDIR="$BUILD_DIR/wine" --all
+		./patchinstall.sh DESTDIR=../../wine --all || patching_error
 	fi
 fi
 
 if [ "$2" = "exit" ] || [ "$3" = "exit" ] || [ "$4" = "exit" ] || [ "$5" = "exit" ] || [ "$6" = "exit" ]; then
-	clear; echo "Force exiting"
+	echo "Force exiting"
 	exit
 fi
 
-make_build_scripts
-cd "$BUILD_DIR" && mv wine "$USERHOME/xenial64_chroot/root" && build_in_chroot xenial64_chroot
-mv "$USERHOME/xenial64_chroot/root/wine-build" "$USERHOME/xenial_chroot/root"
-cp -r "$USERHOME/xenial_chroot/root/wine-build" "$USERHOME/wine-$WINE_VERSION-amd64-nomultilib"
-mv "$USERHOME/xenial64_chroot/root/build64" "$USERHOME/xenial_chroot/root"
-mv "$USERHOME/xenial64_chroot/root/wine" "$USERHOME/xenial_chroot/root" && build_in_chroot xenial_chroot
-mv "$USERHOME/xenial_chroot/root/wine-build" "$USERHOME/wine-$WINE_VERSION-amd64"
-mv "$USERHOME/xenial_chroot/root/wine32-build" "$USERHOME/wine-$WINE_VERSION-x86"
+clear; echo "Creating build scripts"
+create_build_scripts
 
-rm -r "$USERHOME/xenial_chroot/root/build64"
-rm -r "$USERHOME/xenial_chroot/root/build-tools"
-rm -r "$USERHOME/xenial_chroot/root/build-combo"
-rm -r "$USERHOME/xenial_chroot/root/wine"
+clear; echo "Compiling 64-bit Wine"
+sudo cp -r "$SOURCES_DIR/wine" "$CHROOT_X64/opt"
+build_in_chroot 64
 
-cd "$USERHOME" && rm -r "$BUILD_DIR"
+sudo mv "$CHROOT_X64/opt/wine-build" "$CHROOT_X32/opt"
+sudo cp -r "$CHROOT_X32/opt/wine-build" "$MAINDIR/wine-$WINE_VERSION-amd64-nomultilib"
+sudo mv "$CHROOT_X64/opt/build64" "$CHROOT_X32/opt"
 
-cd "$USERHOME/wine-$WINE_VERSION-x86" && rm -r include && rm -r share/applications && rm -r share/man
-cd "$USERHOME/wine-$WINE_VERSION-amd64" && rm -r include && rm -r share/applications && rm -r share/man
-cd "$USERHOME/wine-$WINE_VERSION-amd64-nomultilib" && rm -r include && rm -r share/applications && rm -r share/man && cd bin && ln -sr wine64 wine
-cd "$USERHOME"
+clear; echo "Compiling 32-bit Wine"
+sudo mv "$CHROOT_X64/opt/wine" "$CHROOT_X32/opt"
+build_in_chroot 32
 
-tar -cf wine-$WINE_VERSION-x86.tar wine-$WINE_VERSION-x86
-xz -9 wine-$WINE_VERSION-x86.tar
-rm -r wine-$WINE_VERSION-x86
+clear; echo "Compiling is done. Packing Wine."
+
+sudo mv "$CHROOT_X32/opt/wine-build" "$MAINDIR/wine-$WINE_VERSION-amd64"
+sudo mv "$CHROOT_X32/opt/wine32-build" "$MAINDIR/wine-$WINE_VERSION-x86"
+
+sudo chown -R $USER:$USER "$MAINDIR/wine-$WINE_VERSION-amd64"
+sudo chown -R $USER:$USER "$MAINDIR/wine-$WINE_VERSION-amd64-nomultilib"
+sudo chown -R $USER:$USER "$MAINDIR/wine-$WINE_VERSION-x86"
+
+sudo rm -r "$CHROOT_X64/opt"
+sudo mkdir "$CHROOT_X64/opt"
+sudo rm -r "$CHROOT_X32/opt"
+sudo mkdir "$CHROOT_X32/opt"
+
+cd "$MAINDIR/wine-$WINE_VERSION-x86" && rm -r include && rm -r share/applications && rm -r share/man
+cd "$MAINDIR/wine-$WINE_VERSION-amd64" && rm -r include && rm -r share/applications && rm -r share/man
+cd "$MAINDIR/wine-$WINE_VERSION-amd64-nomultilib" && rm -r include && rm -r share/applications && rm -r share/man && cd bin && ln -sr wine64 wine
+
+cd "$MAINDIR"
 tar -cf wine-$WINE_VERSION-amd64.tar wine-$WINE_VERSION-amd64
-xz -9 wine-$WINE_VERSION-amd64.tar
-rm -r wine-$WINE_VERSION-amd64
 tar -cf wine-$WINE_VERSION-amd64-nomultilib.tar wine-$WINE_VERSION-amd64-nomultilib
+tar -cf wine-$WINE_VERSION-x86.tar wine-$WINE_VERSION-x86
+xz -9 wine-$WINE_VERSION-amd64.tar
 xz -9 wine-$WINE_VERSION-amd64-nomultilib.tar
-rm -r wine-$WINE_VERSION-amd64-nomultilib
+xz -9 wine-$WINE_VERSION-x86.tar
+
+rm -r wine-$WINE_VERSION-*
+
+clear; echo "Done."
