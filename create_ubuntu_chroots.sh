a704ac4049f7966b5ebabcc967477258736f3528
Kron4ek <kron4ek@gmail.com>
2021-08-22 21:22:09 +0500

    Install libxpresent
    Optional dependency for new Proton versions.

--- create_ubuntu_chroots.sh
+++ create_ubuntu_chroots.sh
@@ -91,7 +91,7 @@ add-apt-repository -y ppa:cybermax-dexter/mingw-w64-backport
 apt-get update
 apt-get -y build-dep wine-development libsdl2 libvulkan1
 apt-get -y install ccache gcc-9 g++-9 wget git gcc-mingw-w64 g++-mingw-w64
-apt-get -y install libjxr-dev libusb-1.0-0-dev libgcrypt20-dev libpulse-dev libudev-dev libsane-dev libv4l-dev libkrb5-dev libgphoto2-dev liblcms2-dev libpcap-dev libcapi20-dev
+apt-get -y install libxpresent-dev libjxr-dev libusb-1.0-0-dev libgcrypt20-dev libpulse-dev libudev-dev libsane-dev libv4l-dev libkrb5-dev libgphoto2-dev liblcms2-dev libpcap-dev libcapi20-dev
 apt-get -y purge libvulkan-dev libvulkan1 libsdl2-dev libsdl2-2.0-0 --purge --autoremove
 apt-get -y clean
 apt-get -y autoclean
