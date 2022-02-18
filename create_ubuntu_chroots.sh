c8c2fb5d0551d66fde1ba1e6665340c949a21ce0
liberodark <liberodark@gmail.com>
2022-02-18 09:24:30 +0100

    Bump Packages
    Don't upgrade now FAudio for WMA isssue.

--- create_ubuntu_chroots.sh
+++ create_ubuntu_chroots.sh
@@ -66,10 +66,10 @@ prepare_chroot () {
 }
 
 create_build_scripts () {
-	sdl2_version="2.0.12"
+	sdl2_version="2.0.20"
 	faudio_version="21.12"
-	vulkan_headers_version="1.2.203"
-	vulkan_loader_version="1.2.203"
+	vulkan_headers_version="1.2.205"
+	vulkan_loader_version="1.2.205"
 	spirv_headers_version="1.5.4.raytracing.fixed"
 
 	cat <<EOF > "${MAINDIR}"/prepare_chroot.sh
