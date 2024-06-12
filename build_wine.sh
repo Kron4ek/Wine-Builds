c5bd27828d8cb916723c24eb6d752da8f91fdcef
Kron4ek <kron4ek@gmail.com>
2024-06-12 22:26:05 +0300

    Revert the previous commit

--- build_wine.sh
+++ build_wine.sh
@@ -222,6 +222,18 @@ elif [ "$WINE_BRANCH" = "proton" ]; then
 		git clone https://github.com/ValveSoftware/wine -b "${PROTON_BRANCH}"
 	fi
 
+	if [ "${PROTON_BRANCH}" = "experimental_8.0" ]; then
+		patch -d wine -Np1 < "${scriptdir}"/proton-exp-8.0.patch
+	fi
+
+	if [ "${PROTON_BRANCH}" = "experimental_9.0" ] || [ "${PROTON_BRANCH}" = "bleeding-edge" ]; then
+		patch -d wine -Np1 < "${scriptdir}"/proton-exp-9.0.patch
+	fi
+
+	if [ "${PROTON_BRANCH}" = "proton_9.0" ]; then
+		patch -d wine -Np1 < "${scriptdir}"/proton-9.0.patch
+	fi
+
 	WINE_VERSION="$(cat wine/VERSION | tail -c +14)-$(git -C wine rev-parse --short HEAD)"
 	if [[ "${PROTON_BRANCH}" == "experimental_"* ]] || [ "${PROTON_BRANCH}" = "bleeding-edge" ]; then
 		BUILD_NAME=proton-exp-"${WINE_VERSION}"
