47fc7526c07786f9d1bf3a919f7e50cb7ac290f4
Kron4ek <kron4ek@gmail.com>
2024-06-19 22:05:50 +0300

    Update create_ubuntu_bootstraps.sh

--- create_ubuntu_bootstraps.sh
+++ create_ubuntu_bootstraps.sh
@@ -73,6 +73,8 @@ create_build_scripts () {
 	spirv_headers_version="sdk-1.3.239.0"
  	libpcap_version="1.10.4"
   	libxkbcommon_version="1.6.0"
+   	python3_version="3.12.4"
+    	meson_version="1.3.2"
 
 	cat <<EOF > "${MAINDIR}"/prepare_chroot.sh
 #!/bin/bash
@@ -96,17 +98,15 @@ apt-get -y install software-properties-common
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 add-apt-repository -y ppa:cybermax-dexter/mingw-w64-backport
 apt-get update
-apt-get -y build-dep wine-development libsdl2 libvulkan1
+apt-get -y build-dep wine-development libsdl2 libvulkan1 python3
 apt-get -y install ccache gcc-9 g++-9 wget git gcc-mingw-w64 g++-mingw-w64
 apt-get -y install libxpresent-dev libjxr-dev libusb-1.0-0-dev libgcrypt20-dev libpulse-dev libudev-dev libsane-dev libv4l-dev libkrb5-dev libgphoto2-dev liblcms2-dev libcapi20-dev
 apt-get -y install libjpeg62-dev samba-dev
 apt-get -y install libpcsclite-dev libcups2-dev
-apt-get -y install python3-pip libxcb-xkb-dev
+apt-get -y install python3-pip libxcb-xkb-dev libbz2-dev
 apt-get -y purge libvulkan-dev libvulkan1 libsdl2-dev libsdl2-2.0-0 libpcap0.8-dev libpcap0.8 --purge --autoremove
 apt-get -y clean
 apt-get -y autoclean
-pip3 install meson
-pip3 install ninja
 export PATH="/usr/local/bin:${PATH}"
 mkdir /opt/build_libs
 cd /opt/build_libs
@@ -117,7 +117,10 @@ wget -O vulkan-headers.tar.gz https://github.com/KhronosGroup/Vulkan-Headers/arc
 wget -O spirv-headers.tar.gz https://github.com/KhronosGroup/SPIRV-Headers/archive/${spirv_headers_version}.tar.gz
 wget -O libpcap.tar.gz https://www.tcpdump.org/release/libpcap-${libpcap_version}.tar.gz
 wget -O libxkbcommon.tar.xz https://xkbcommon.org/download/libxkbcommon-${libxkbcommon_version}.tar.xz
+wget -O python3.tar.gz https://www.python.org/ftp/python/${python3_version}/Python-${python3_version}.tgz
+wget -O meson.tar.gz https://github.com/mesonbuild/meson/releases/download/${meson_version}/meson-${meson_version}.tar.gz
 wget -O /usr/include/linux/ntsync.h https://raw.githubusercontent.com/zen-kernel/zen-kernel/f787614c40519eb2c8ebdc116b2cd09d46e5ec85/include/uapi/linux/ntsync.h
+wget -O /usr/include/linux/userfaultfd.h https://raw.githubusercontent.com/zen-kernel/zen-kernel/f787614c40519eb2c8ebdc116b2cd09d46e5ec85/include/uapi/linux/userfaultfd.h
 if [ -d /usr/lib/i386-linux-gnu ]; then wget -O wine.deb https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_4.0.3~bionic_i386.deb; fi
 if [ -d /usr/lib/x86_64-linux-gnu ]; then wget -O wine.deb https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-amd64/wine-stable_4.0.3~bionic_amd64.deb; fi
 git clone git://source.winehq.org/git/vkd3d.git
@@ -128,6 +131,8 @@ tar xf vulkan-headers.tar.gz
 tar xf spirv-headers.tar.gz
 tar xf libpcap.tar.gz
 tar xf libxkbcommon.tar.xz
+tar xf meson.tar.gz -C /usr/local
+ln -s /usr/local/meson-${meson_version}/meson.py /usr/local/bin/meson
 export CFLAGS="-O2"
 export CXXFLAGS="-O2"
 mkdir build && cd build
@@ -149,6 +154,11 @@ cd ../ && rm -r build && mkdir build && cd build
 ../vkd3d/configure && make -j$(nproc) && make install
 cd ../ && rm -r build && mkdir build && cd build
 ../libpcap-${libpcap_version}/configure && make -j$(nproc) install
+cd ../ && rm -r build && mkdir build && cd build
+../Python-${python3_version}/configure --enable-optimizations
+make -j$(nproc)
+make -j$(nproc) install
+pip3 install setuptools
 cd ../libxkbcommon-${libxkbcommon_version}
 meson setup build -Denable-docs=false
 meson compile -C build
