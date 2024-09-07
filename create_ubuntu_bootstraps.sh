5aa678a597ffc6d653033a44011f049640b96ee5
Kron4ek <kron4ek@gmail.com>
2024-09-07 12:01:25 +0300

    Update cmake, ccache and Vulkan-Loader

--- create_ubuntu_bootstraps.sh
+++ create_ubuntu_bootstraps.sh
@@ -68,13 +68,15 @@ prepare_chroot () {
 create_build_scripts () {
 	sdl2_version="2.26.4"
 	faudio_version="23.03"
-	vulkan_headers_version="1.3.239"
-	vulkan_loader_version="1.3.239"
+	vulkan_headers_version="1.3.295"
+	vulkan_loader_version="1.3.295"
 	spirv_headers_version="sdk-1.3.239.0"
  	libpcap_version="1.10.4"
   	libxkbcommon_version="1.6.0"
    	python3_version="3.12.4"
-    meson_version="1.3.2"
+    	meson_version="1.3.2"
+     	cmake_version="3.30.3"
+      	ccache_version="4.10.2"
 
 	cat <<EOF > "${MAINDIR}"/prepare_chroot.sh
 #!/bin/bash
@@ -121,6 +123,8 @@ wget -O libxkbcommon.tar.xz https://xkbcommon.org/download/libxkbcommon-${libxkb
 wget -O python3.tar.gz https://www.python.org/ftp/python/${python3_version}/Python-${python3_version}.tgz
 wget -O meson.tar.gz https://github.com/mesonbuild/meson/releases/download/${meson_version}/meson-${meson_version}.tar.gz
 wget -O mingw.tar.xz http://techer.pascal.free.fr/Red-Rose_MinGW-w64-Toolchain/Red-Rose-MinGW-w64-Posix-Urct-v12.0.0.r0.g819a6ec2e-Gcc-11.4.1.tar.xz
+wget -O cmake.tar.gz https://github.com/Kitware/CMake/releases/download/v${cmake_version}/cmake-${cmake_version}.tar.gz
+wget -O ccache.tar.gz https://github.com/ccache/ccache/releases/download/v${ccache_version}/ccache-${ccache_version}.tar.gz
 wget -O /usr/include/linux/ntsync.h https://raw.githubusercontent.com/zen-kernel/zen-kernel/f787614c40519eb2c8ebdc116b2cd09d46e5ec85/include/uapi/linux/ntsync.h
 wget -O /usr/include/linux/userfaultfd.h https://raw.githubusercontent.com/zen-kernel/zen-kernel/f787614c40519eb2c8ebdc116b2cd09d46e5ec85/include/uapi/linux/userfaultfd.h
 if [ -d /usr/lib/i386-linux-gnu ]; then wget -O wine.deb https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_4.0.3~bionic_i386.deb; fi
@@ -134,12 +138,21 @@ tar xf spirv-headers.tar.gz
 tar xf libpcap.tar.gz
 tar xf libxkbcommon.tar.xz
 tar xf python3.tar.gz
+tar xf cmake.tar.gz
+tar xf ccache.tar.gz
 tar xf mingw.tar.xz -C /
 tar xf meson.tar.gz -C /usr/local
 ln -s /usr/local/meson-${meson_version}/meson.py /usr/local/bin/meson
+export CC=gcc-11
+export CXX=g++-11
 export CFLAGS="-O2"
 export CXXFLAGS="-O2"
-mkdir build && cd build
+cd cmake-${cmake_version}
+./bootstrap --parallel=$(nproc)
+make -j$(nproc) install
+cd ../ && mkdir build && cd build
+cmake ../ccache-${ccache_version} && make -j$(nproc) && make install
+cd ../ && rm -r build && mkdir build && cd build
 cmake ../SDL2-${sdl2_version} && make -j$(nproc) && make install
 cd ../ && rm -r build && mkdir build && cd build
 cmake ../FAudio-${faudio_version} && make -j$(nproc) && make install
