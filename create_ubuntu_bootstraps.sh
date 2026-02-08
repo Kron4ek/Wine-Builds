4cc7d0b1fcf0bf6cf09897cdf13436c6b9100a77
Kron4ek <kron4ek@gmail.com>
2026-02-09 00:15:21 +0300

    Update wayland libraries and protocols
    And some other libraries.
    
    #222

--- create_ubuntu_bootstraps.sh
+++ create_ubuntu_bootstraps.sh
@@ -66,19 +66,21 @@ prepare_chroot () {
 }
 
 create_build_scripts () {
-	sdl2_version="2.26.4"
+	sdl2_version="2.32.10"
 	faudio_version="23.03"
-	vulkan_headers_version="1.4.328"
-	vulkan_loader_version="1.4.328"
+	vulkan_headers_version="1.4.343"
+	vulkan_loader_version="1.4.343"
 	spirv_headers_version="sdk-1.3.239.0"
  	libpcap_version="1.10.4"
-  	libxkbcommon_version="1.6.0"
+  	libxkbcommon_version="1.13.1"
    	python3_version="3.12.4"
     meson_version="1.3.2"
     cmake_version="3.30.3"
-    ccache_version="4.12.1"
+    ccache_version="4.12.3"
     libglvnd_version="1.7.0"
 	bison_version="3.8.2"
+	wayland_version="1.24.0"
+	wayland_protocols_version="1.47"
 
 	cat <<EOF > "${MAINDIR}"/prepare_chroot.sh
 #!/bin/bash
@@ -108,6 +110,7 @@ apt-get -y install libxpresent-dev libjxr-dev libusb-1.0-0-dev libgcrypt20-dev l
 apt-get -y install libjpeg62-dev samba-dev
 apt-get -y install libpcsclite-dev libcups2-dev
 apt-get -y install python3-pip libxcb-xkb-dev libbz2-dev texinfo curl
+apt-get -y install graphviz xmlto --no-install-recommends
 apt-get -y purge libvulkan-dev libvulkan1 libsdl2-dev libsdl2-2.0-0 libpcap0.8-dev libpcap0.8 --purge --autoremove
 apt-get -y purge *gstreamer* --purge --autoremove
 apt-get -y clean
@@ -121,13 +124,15 @@ wget -O vulkan-loader.tar.gz https://github.com/KhronosGroup/Vulkan-Loader/archi
 wget -O vulkan-headers.tar.gz https://github.com/KhronosGroup/Vulkan-Headers/archive/v${vulkan_headers_version}.tar.gz
 wget -O spirv-headers.tar.gz https://github.com/KhronosGroup/SPIRV-Headers/archive/${spirv_headers_version}.tar.gz
 wget -O libpcap.tar.gz https://www.tcpdump.org/release/libpcap-${libpcap_version}.tar.gz
-wget -O libxkbcommon.tar.xz https://xkbcommon.org/download/libxkbcommon-${libxkbcommon_version}.tar.xz
+wget -O libxkbcommon.tar.gz https://github.com/xkbcommon/libxkbcommon/archive/refs/tags/xkbcommon-${libxkbcommon_version}.tar.gz
 wget -O python3.tar.gz https://www.python.org/ftp/python/${python3_version}/Python-${python3_version}.tgz
 wget -O meson.tar.gz https://github.com/mesonbuild/meson/releases/download/${meson_version}/meson-${meson_version}.tar.gz
 wget -O cmake.tar.gz https://github.com/Kitware/CMake/releases/download/v${cmake_version}/cmake-${cmake_version}.tar.gz
 wget -O ccache.tar.gz https://github.com/ccache/ccache/releases/download/v${ccache_version}/ccache-${ccache_version}.tar.gz
 wget -O libglvnd.tar.gz https://gitlab.freedesktop.org/glvnd/libglvnd/-/archive/v${libglvnd_version}/libglvnd-v${libglvnd_version}.tar.gz
 wget -O bison.tar.xz https://ftp.gnu.org/gnu/bison/bison-${bison_version}.tar.xz
+wget -O wayland.tar.xz https://gitlab.freedesktop.org/wayland/wayland/-/releases/${wayland_version}/downloads/wayland-${wayland_version}.tar.xz
+wget -O wayland-protocols.tar.xz https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/${wayland_protocols_version}/downloads/wayland-protocols-${wayland_protocols_version}.tar.xz
 wget -O /usr/include/linux/ntsync.h https://raw.githubusercontent.com/zen-kernel/zen-kernel/refs/heads/6.15/main/include/uapi/linux/ntsync.h
 wget -O /usr/include/linux/userfaultfd.h https://raw.githubusercontent.com/zen-kernel/zen-kernel/refs/heads/6.15/main/include/uapi/linux/userfaultfd.h
 if [ -d /usr/lib/i386-linux-gnu ]; then wget -O wine.deb https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_4.0.3~bionic_i386.deb; fi
@@ -140,12 +145,14 @@ tar xf vulkan-loader.tar.gz
 tar xf vulkan-headers.tar.gz
 tar xf spirv-headers.tar.gz
 tar xf libpcap.tar.gz
-tar xf libxkbcommon.tar.xz
+tar xf libxkbcommon.tar.gz
 tar xf python3.tar.gz
 tar xf cmake.tar.gz
 tar xf ccache.tar.gz
 tar xf libglvnd.tar.gz
 tar xf bison.tar.xz
+tar xf wayland.tar.xz
+tar xf wayland-protocols.tar.xz
 tar xf meson.tar.gz -C /usr/local
 ln -s /usr/local/meson-${meson_version}/meson.py /usr/local/bin/meson
 bash mingw-w64-build x86_64
@@ -180,21 +187,29 @@ cd ../ && rm -r build && mkdir build && cd build
 make -j$(nproc)
 make -j$(nproc) install
 pip3 install setuptools
-cd ../libxkbcommon-${libxkbcommon_version}
-meson setup build -Denable-docs=false
-meson compile -C build
-meson install -C build
 cd ../gstreamer
 meson setup build
 ninja -C build
 ninja -C build install
+cd ../bison-${bison_version}
+./configure
+make -j$(nproc) install
+cd ../wayland-${wayland_version}
+meson setup build
+meson compile -C build
+meson install -C build
+cd ../wayland-protocols-${wayland_protocols_version}
+meson setup build
+meson compile -C build
+meson install -C build
+cd ../libxkbcommon-xkbcommon-${libxkbcommon_version}
+meson setup build -Denable-docs=false
+meson compile -C build
+meson install -C build
 cd ../libglvnd-v${libglvnd_version}
 meson setup build
 meson compile -C build
 meson install -C build
-cd ../bison-${bison_version}
-./configure
-make -j$(nproc) install
 cd /opt && rm -r /opt/build_libs
 EOF
 
